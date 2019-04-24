5782fe521cdf742d87b5d3947076cfbcff2a92d1
zero <1137774080@qq.com>
2019-04-24 20:32:15 +0800

    连接详情

--- app/pages/seach/seach.js
+++ app/pages/seach/seach.js
@@ -35,6 +35,14 @@ Page({
       }
     ]
   },
+  to_res(){
+    wx.navigateTo({
+      url: '/pages/details/details',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
   bindMultiPickerChange(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
